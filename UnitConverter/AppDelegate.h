9c2ef4794ad52f42f7faf21828e107796e7a8431
Ruud Schouwenaar <ruudschouwenaar@macbook-pro-van-ruud.local>
2016-01-15 15:05:34 +0100

    Initial Commit

---
+++ UnitConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitConverter
+//
+//  Created by Ruud Schouwenaar on 15-01-16.
+//  Copyright © 2016 Ruud Schouwenaar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnitConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitConverter
+//
+//  Created by Ruud Schouwenaar on 15-01-16.
+//  Copyright © 2016 Ruud Schouwenaar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
